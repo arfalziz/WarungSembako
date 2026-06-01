88b80feb5b60b237005d98c172b36d0544813a0f
arfalziz <arfalziz@github.com>
2026-06-01 20:37:22 +0700

    Implement product module (Phase 3)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { FastifyInstance } from 'fastify';
 import { authRoutes } from '../controllers/auth';
+import { productRoutes } from '../controllers/products';
 
 export default async function router(fastify: FastifyInstance) {
   fastify.register(authRoutes, { prefix: '/api/v1/auth' });
+  fastify.register(productRoutes, { prefix: '/api/v1/products' });
   
   // Future route groups can be registered here:
-  // fastify.register(productRoutes, { prefix: '/api/v1/products' });
   // fastify.register(orderRoutes, { prefix: '/api/v1/orders' });
 }
